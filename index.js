7197470f867766a2fe49c1918532fb704d999229
Eric Juta <ericjuta@MAC048212.local>
2016-01-02 20:29:10 +0000

    Added list my tickets and list all commands to Taxman - Connected taxman to pivotaltracker!!

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 var Botkit = require('botkit')
+var tracker = require('pivotaltracker')
+var R = require('ramda')
 var bot = Botkit.slackbot()
+var pivotal = new tracker.Client(process.env.pttoken)
 
 bot.spawn({
   token: process.env.token
@@ -7,11 +10,37 @@ bot.spawn({
   if(err) {throw new Error(err)}
 })
 
-bot.hears('hello','direct_message,direct_mention,mention', function(bot, message) {
-  // bot.startConversation(message,function(err,convo) {
-
-  //   convo.say('All your vitamin c are belong to us.');
-
-  // })
-  bot.reply(message, 'All your vitamin c are belong to us.')
+/*
+ @taxman list all
+ PMs you a link with the pivotal tracker project to view all the tickets in the web UI
+ */
+bot.hears('list all','direct_message,direct_mention,mention', function(bot, message) {
+  bot.startPrivateConversation(message, function(err, convo) {
+    convo.say("You can view all the project stories here: https://www.pivotaltracker.com/n/projects/1479718/")
+  })
 })
+
+
+/*
+ @taxman list my tickets
+ PMs your personal assigned tickets
+ */
+bot.hears('list my tickets','direct_message,direct_mention,mention', function(bot, message) {
+  bot.startPrivateConversation(message, function(err, convo) {
+    pivotal.project(1479718).stories.all(function(err, stories) {
+      var ownedStories = R.filter( function(story) {
+        return (story.requestedById == 1850810 || R.contains(1850810, story.ownerIds))
+      }, stories)
+      convo.say("Here are your requested tickets:")
+      ownedStories.map(function(story) {
+        convo.say("id: " + story.id + "\n" +
+                  "url: " + story.url + "\n" +
+                  "type: " + story.type + "\n" +
+                  "point estimate: " + story.estimate + "\n" +
+                  "name: " + story.name + "\n" +
+                  "description: " + story.description)
+      })
+    })
+  })
+})
+// pivotal.project(1479718).memberships.all(function(err, all) {console.log(all)})
