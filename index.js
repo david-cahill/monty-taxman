84f8f4a594bba76189d53d63ee6efe8d4c0a7cfb
Eric Juta <ericjuta@MAC048212.local>
2016-01-02 20:48:29 +0000

    Changed returned tickets to be only those available

--- index.js
+++ index.js
@@ -25,11 +25,11 @@ bot.hears('list all','direct_message,direct_mention,mention', function(bot, mess
  @taxman list my tickets
  PMs your personal assigned tickets
  */
-bot.hears('list my tickets','direct_message,direct_mention,mention', function(bot, message) {
+bot.hears(['list my tickets', 'list available'],'direct_message,direct_mention,mention', function(bot, message) {
   bot.startPrivateConversation(message, function(err, convo) {
     pivotal.project(1479718).stories.all(function(err, stories) {
       var ownedStories = R.filter( function(story) {
-        return (story.requestedById == 1850810 || R.contains(1850810, story.ownerIds))
+        return (story.requestedById == 1850810 || R.contains(1850810, story.ownerIds) && story.currentState != "accepted" || "delivered" || "finished")
       }, stories)
       convo.say("Here are your requested tickets:")
       ownedStories.map(function(story) {
